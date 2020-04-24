6f0dc2c4e758850154055efe745ed736d3c9c4d4
Nader Akhgari <nader90akhgari@gmail.com>
2020-04-24 01:34:03 +0100

    I did level-200 of tv-show

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  searchEpisode(allEpisodes);
 }
 
 function headerData(episodeObj){//this function colects the data for header of episodes.
@@ -12,15 +13,26 @@ function headerData(episodeObj){//this function colects the data for header of e
   return `${episodeObj.name} - S${seasonNum}E${episodeNum}`
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  episodeList.forEach(episode => {//Iterate through the array of episode.
+const rootElem = document.getElementById("root");
+let searchRoot = document.createElement("div");
+let episodeRoot = document.createElement("div");
 
+rootElem.className = "mainDiv";
+searchRoot.className = "searchRoot";
+episodeRoot.className = "episodeRoot";
+
+rootElem.appendChild(searchRoot);
+rootElem.appendChild(episodeRoot);
+
+function makePageForEpisodes(episodeList) {
+  episodeRoot.innerHTML = "";
+  
+   episodeList.forEach(episode => {//Iterate through the array of episode.
     let summaryText = episode.summary.replace(/<p>/g, " ");//remove the '<p>' from the summary.
     let pEl = summaryText.replace(/<\/p>/g, " ");//remove the '</p>' from the summary.
 
-    //Create the elements by innerHTML and add to root div.
-    rootElem.innerHTML += `
+    //Create the elements by innerHTML and add to episodeRoot div.
+    episodeRoot.innerHTML += `
     <div class= "divElStyle">
       <h2 class = "h2ElStyle ">${headerData(episode)}</h2>
       <img src = ${episode.image.medium}>
@@ -29,6 +41,39 @@ function makePageForEpisodes(episodeList) {
   })
 }
 
+function searchEpisode(episodeList){// This function will create search part.
+  let searchDiv = document.createElement("div");
+  let searchText = document.createElement("input");
+  let searchState = document.createElement("label");
+  searchState.innerHTML = `Display ${episodeList.length}/${episodeList.length} episodes`;
+
+  searchText.type = "text";
+  searchText.className = "searchInText";
+  searchState.className = "searchState";
+  searchDiv.className = "searchDiv";
+
+  searchRoot.appendChild(searchDiv);
+  searchDiv.appendChild(searchText);
+  searchDiv.appendChild(searchState);
+
+  function myFunction(){// This function will comare search text with episodes summary and name
+    const filteredEpisodeList = episodeList.filter(episode => {
+      let lowerSummary = episode.summary.toLowerCase();
+      let lowerHeader = headerData(episode).toLowerCase();
+      let lowerSearch = searchText.value.toLowerCase();
+      if(lowerSummary.indexOf(lowerSearch)>-1 || lowerHeader.indexOf(lowerSearch) > -1){
+        return true;
+      } else{
+        return false;
+      }
+    })
+    searchState.innerHTML = `Display ${filteredEpisodeList.length}/${episodeList.length} episodes`;
+    makePageForEpisodes(filteredEpisodeList);
+  };
+  searchText.addEventListener('input',myFunction);
+}
 
 window.onload = setup;
+
+
 
